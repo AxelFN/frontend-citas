7e89798144953cbc84b9ec559cb8488582939120
AxelFN <130577655+AxelFN@users.noreply.github.com>
2025-07-16 18:01:15 -0600

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       } catch (error) {
         console.error("Error al iniciar sesión:", error);
-        alert("Error del servidor");
+        alert("NEL");
       }
     });
   }
